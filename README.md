4ae9b60bdb1c040b162c02d689dad423de17fd07
MO Fei <mo-fei@outlook.com>
2020-02-03 17:47:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# website-vue-golang
+ A website using Vue.js as front-end and Beego as back-end.
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "website-vue-golang/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
